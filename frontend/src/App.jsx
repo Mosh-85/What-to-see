2b61173f44c5c5b2fa6d8555fa5d6f7165e8cd0c
Camilla H. Larsen <millalar@yahoo.no>
2024-04-23 15:47:38 +0200

    FELLES: SASS: oppsett
    Laget oppsett av SASS for hvert komponent. Samt satt struktur for import av SASS.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
+import "./style/main.scss"
 
 function App() {
   return (
