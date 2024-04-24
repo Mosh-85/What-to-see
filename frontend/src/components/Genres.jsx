13ea872f65888bdcbebe7346368ab9a987a12fb5
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-04-24 12:41:39 +0200

    lagt til check box på genre
    check box på genre forberedelse for å sende til sanity

--- frontend/src/components/Genres.jsx
+++ frontend/src/components/Genres.jsx
@@ -1,12 +1,21 @@
 import { fetchAllGenres } from "../../sanity/services.js/genreServices";
 import { useEffect, useState } from "react";
+import Checkbox from "@mui/material/Checkbox";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
 
 export default function Genres() {
   const [genres, setGenres] = useState([]);
+  const label = { inputProps: { "aria-label": "Fav-Toggle" } };
 
   const getAllGenres = async () => {
     const data = await fetchAllGenres();
     setGenres(data);
+  };
+
+  const handleCheckboxChange = (e, i) => {
+    console.log(e.target.checked);
+    console.log(i);
   };
 
   useEffect(() => {
@@ -20,7 +29,15 @@ export default function Genres() {
       <h1>Genres</h1>
       <ul>
         {genres.map((genre, i) => (
-          <li key={i}>{genre.name}</li>
+          <li key={i}>
+            {genre.name}
+            <Checkbox
+              {...label}
+              icon={<FavoriteBorder />}
+              checkedIcon={<Favorite />}
+              onChange={(e) => handleCheckboxChange(e, i)}
+            />
+          </li>
         ))}
       </ul>
     </section>
