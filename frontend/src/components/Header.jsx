e52ae7db701d8e17adfa3969d0bc33a819593517
Camilla H. Larsen <millalar@yahoo.no>
2024-04-25 11:29:04 +0200

    Opprettet link til sidene

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -28,12 +28,11 @@ export default function Header({ logedIn, setLogedIn }) {
   return (
     <header className="header">
       <nav>
-        <Button className="logo" href="#text-buttons">What To See?</Button>
-        <Button className="whattosee" startIcon={<TvIcon/>} href="#text-buttons" >Hva skal jeg se?</Button>
-        <Button className="genre" href="#text-buttons">Bla gjennom sjangere</Button>
+        <Button className="logo" href="./">What To See?</Button>
+        <Button className="whattosee" startIcon={<TvIcon/>} href="./home" >Hva skal jeg se?</Button>
+        <Button className="genre" href="./Genres">Bla gjennom sjangere</Button>
        
         <Button className="user"
-        id="demo-positioned-button"
         aria-controls={open ? 'demo-positioned-menu' : undefined}
         aria-haspopup="true"
         aria-expanded={open ? 'true' : undefined}
@@ -42,7 +41,6 @@ export default function Header({ logedIn, setLogedIn }) {
        {user}
       </Button>
       <Menu
-        id="demo-positioned-menu"
         aria-labelledby="demo-positioned-button"
         anchorEl={anchorEl}
         open={open}
