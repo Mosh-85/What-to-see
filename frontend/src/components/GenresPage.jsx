0455ac67da2e49dc167b286a64f0f0f01eb3b459
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-05-07 14:56:29 +0200

    fetch movies by slug

--- frontend/src/components/GenresPage.jsx
+++ frontend/src/components/GenresPage.jsx
@@ -1,25 +1,42 @@
 import { useEffect, useState } from "react";
-import { useParams} from "react-router-dom";
+import { Link, useParams} from "react-router-dom";
+import { fetchMoviesBySlug } from "../../sanity/services.js/genreServices";
 
 export default function GenresPage() {
 
     const { slug } = useParams();
-    const [genre, setGenre] = useState(null);
+    const upperslug = slug.charAt(0).toUpperCase() + slug.slice(1);
+    const [moviesByGenre, setMoviesByGenre] = useState(null);
   
-    const getMoviesBySlug = async (slug) => {
-      const data = await fetchGenreBySlug(slug);
-      setGenre(data[0]);
+    const getMoviesBySlug = async () => {
+      const data = await fetchMoviesBySlug(upperslug);
+      setMoviesByGenre(data);
     };
   
     useEffect(() => {
       getMoviesBySlug(slug);
     }, [slug]);
 
+
+      const listMoviesByGenre = moviesByGenre?.map((movies, index) => {
+        return (
+          <li key={index}>
+            <img src={movies.title} alt={movies.title + " " + "bilde"} />
+            <a href={`https://www.imdb.com/title/${movies.imdbid}`}> {movies.title}</a> 
+          </li>
+        )
+      }) 
+
+
     return (
-        <section>
-            <h1>{slug}</h1>
-        </section>
-
-
+      <section>
+        {listMoviesByGenre && (
+          <>
+            <h3>Sjanger: {upperslug} ({listMoviesByGenre.length} filmer) </h3>
+            <ul> {listMoviesByGenre} </ul>
+            
+          </>
+        )}
+      </section>
     );
 }
