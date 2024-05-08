7e7599e0d4d90423085b62d7c405ed71a7be8a16
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-05-08 13:32:54 +0200

    oppdatert

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 export default function MovieCard({ movies, index }) {
 
- 
+    const [imdbImage, setImdImage] = useState();
+
+    const fetchImdImage = async (movieId) => {
+
+        const url = `https://moviesdatabase.p.rapidapi.com/titles/${movies.imdbId}`;
+        try {
+            const response = await fetch(url,apiClient);
+            const result = await response.json();
+            setImdImage(
+                {
+                title: result.results.titleText.text,
+                image: result.results.primaryImage.url
+            })
+        } catch (error) { console
+            .error(error);
+        }
+
+    }
+
+    useEffect(() => {
+        fetchImdImage(movies.imdbId);
+    }, [movies.imdbId])
+
+    console.log(imdbImage);
 
   return (
     <>
