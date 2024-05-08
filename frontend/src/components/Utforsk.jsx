e6a1768466e97630a47a76f2f96f10f6cb31a6ba
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-05-08 15:15:00 +0200

    oppdatert

--- frontend/src/components/Utforsk.jsx
+++ frontend/src/components/Utforsk.jsx
@@ -1,11 +1,64 @@
-import Genres from "./Genres"
+import { useState, useEffect } from 'react';
+import { fetchFavGenres } from '../../sanity/services.js/userServices';
 
-export default function Utforsk() {
+export default function Utforsk({userId, user2Id}) {
+
+    const [favGenres, setFavGenres] = useState(null)
+    const [favGenres2, setFavGenres2] = useState(null)
+
+    console.log("Userid",userId)
+    console.log("User2id",user2Id)
+    console.log("FavGenres",favGenres)
+    console.log("FavGenres2",favGenres2)
+
+
+
+    useEffect(() => { 
+     const fetchData = async () => {
+     const data = await fetchFavGenres(userId)
+     setFavGenres(data[0].favoriteGenresName);
+    }
+     fetchData();
+    }, [userId]);
+
+
+   useEffect(() => { 
+    const fetchData = async () => {
+    const data = await fetchFavGenres(user2Id)
+    setFavGenres2(data[0].favoriteGenresName);
+   }
+    fetchData();
+   }, [user2Id]);
+
+   const listCommonFavGenres = favGenres?.filter(movie1 =>
+    favGenres2?.some(movie2 => movie2.title === movie1.title)
+  ).map((movies, index) => {
     return (
+      <ul>
+        <li>
+            {listCommonFavGenres}
+        </li>
+      </ul>
+    );
+  });
+console.log(listCommonFavGenres)
+
+   return (
+    <section>
+        {listCommonFavGenres && (
     <>
-        <h3>Utforsk!</h3>
-        <p>Dere liker begge disse sjangerne. Sjekk hvilke filmer som finnes ved å velge mellom:</p>
+      
+        <h3>Deres ønskeliste!</h3>
+        <p>Dere har {listCommonFavGenres.length} film(er) felles i ønskelisten deres</p>
+        
+            <li>
+                {listCommonFavGenres}
+            </li>
+
+
         
     </>
+)}
+    </section>
     )
 }
