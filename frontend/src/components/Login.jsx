774af9e037a5cc8cda2f175a06a074bb678e85c2
Camilla H. Larsen <millalar@yahoo.no>
2024-05-10 01:56:43 +0200

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,27 +1,22 @@
 export default function Login({ users, setLogedIn}) {
   
-
   return (
     <section className="login">
       <h3>Hvem skal se i dag?</h3>
       <p>Velg bruker:</p>
-
       <ul>
         {users.map((user, i) => (
           <li key={i}>
-            <button
-              variant="contained"
-              onClick={() => {
-                localStorage.setItem("loggedInUser", user._id);
-                localStorage.setItem("loggedInUserName", user.name);
-                setLogedIn(user.name);
-              }}
-            >
+            <button variant="contained" onClick={() => {
+                localStorage.setItem("loggedInUser", user._id)
+                localStorage.setItem("loggedInUserName", user.name)
+                setLogedIn(user.name)
+              }}>
               {user.name}
             </button>
           </li>
         ))}
       </ul>
     </section>
-  );
+  )
 }
