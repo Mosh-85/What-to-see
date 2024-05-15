7d81250d8bbee7477cc73e66826374914163cbcb
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-05-15 16:24:25 +0200

    oppdatert

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,7 +8,7 @@ export default function Login({ users, setLogedIn}) {
       <p>Velg bruker:</p>
       <div className="user-list">
         {users.map((user, i) => (
-            <button key={i} variant="contained" onClick={() => {
+            <button className="user-list" key={i} variant="contained" onClick={() => {
                 localStorage.setItem("loggedInUser", user._id)
                 localStorage.setItem("loggedInUserName", user.name)
                 setLogedIn(user.name)
