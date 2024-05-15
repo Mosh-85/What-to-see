395ef9e205b58a7086770a5467978af782dadadf
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-05-15 22:11:52 +0200

    Update MovieCard.jsx

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -41,7 +41,7 @@ export default function MovieCard({ movies, index }) {
 
 
   return (
-  <>
+    <>
         <li key={index} className="movie-card" >
         <img src={api?.image} alt={api?.title + " " + "bilde"} width={300} height={420}/>
         <a href={`https://www.imdb.com/title/${movies?.imdbid}`} target= "_blank"> {api?.title} ({api?.release})</a> 
