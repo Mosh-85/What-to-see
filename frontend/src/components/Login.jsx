81d1849dd4b369209db176172abbee8aa1417897
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-05-15 11:50:23 +0200

    fjernet ul li fran button
    lagt til div rundt button for styling.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,22 +1,25 @@
+import React from 'react';
+
 export default function Login({ users, setLogedIn}) {
   
   return (
     <section className="login">
       <h3>Hvem skal se i dag?</h3>
       <p>Velg bruker:</p>
-      <ul>
+      <div className="user-list">
         {users.map((user, i) => (
-          <li key={i}>
-            <button variant="contained" onClick={() => {
+            <button key={i} variant="contained" onClick={() => {
                 localStorage.setItem("loggedInUser", user._id)
                 localStorage.setItem("loggedInUserName", user.name)
                 setLogedIn(user.name)
               }}>
               {user.name}
             </button>
-          </li>
         ))}
-      </ul>
+      </div>
     </section>
   )
 }
+
+
+
